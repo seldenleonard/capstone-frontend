bf20cf1cc87407ae79f2302296d79285ff3ba0bc
seldenleonard <selden222@gmail.com>
2020-11-03 11:08:43 -0800

    adds Signup.vue component, route, and router-link

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Signup from "../views/Signup.vue";
 
 Vue.use(VueRouter)
 
@@ -17,13 +18,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
