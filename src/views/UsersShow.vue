9b291e066285a76d1f9be05626b4b12bbc8379f1
seldenleonard <selden222@gmail.com>
2020-11-05 23:09:12 -0800

    updates UsersShow.vue

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -10,12 +10,25 @@
     <p>{{ user.major }}</p>
     <p>{{ user.minor }}</p>
     <p>{{ user.graduation_year }}</p>
-
-    <!-- Note: I will need to put a v:if in my routerlink tag to say that if current user id from local storage matches the id of the user show, then the edit button will be available -->
     <router-link :to="`/users/${user.id}/edit`">
       <!-- This^ means same as v-bind -->
       <button>Edit</button>
     </router-link>
+    <div v-for="artwork in user.artworks">
+      <img src="artwork.image" alt="" />
+      <!-- NEED HELP GETTING IMAGES TO SHOW UP -->
+      <h2>{{ artwork.title }}</h2>
+      <p>{{ artwork.medium }}</p>
+      <p>{{ artwork.description }}</p>
+      <p>{{ artwork.price }}</p>
+      <p>{{ artwork.dimensions }}</p>
+      <p>{{ artwork.year }}</p>
+      <!-- Note: I will need to put a v:if in my routerlink tag to say that if current user id from local storage matches the id of the user show, then the edit button will be available (need to do this for the user edit button above as well). it will look something like this: v-if="artwork.user.id === $parent.getUserId()" -->
+      <router-link :to="`/artworks/${artwork.id}/edit`">
+        <!-- This^ means same as v-bind -->
+        <button>Edit</button>
+      </router-link>
+    </div>
   </div>
 </template>
 
