2215dbe960e0704d0d9c571f4aeddb94f2b7ff62
seldenleonard <selden222@gmail.com>
2020-11-13 02:28:24 -0800

    adds artist checkbox and subsequent conditional rendering of other attributes to Signup.vue

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -29,44 +29,53 @@
         <label
           >Are you a student artist attending a college or university?
         </label>
-        <input type="boolean" class="form-control" v-model="artist" />
+        <input
+          type="checkbox"
+          id="checkbox"
+          class="form-control"
+          v-model="artist"
+        />
+        <label for="checkbox">{{ artist }}</label>
       </div>
       <!-- Need to make this have two checkboxes, with "yes" or "no" -->
       <!-- If the user selects no, then nothing happens, but if the user selects "yes", the rest of the inputs (below) will appear -->
-      <div class="form-group">
-        <label>Style of Art:</label>
-        <input type="text" class="form-control" v-model="artStyle" />
-      </div>
-      <div class="form-group">
-        <label>Name of School:</label>
-        <input type="text" class="form-control" v-model="college_id" />
-      </div>
-      <!-- Need to make this a dropdown menu -->
-      <!-- also need to add a note saying "If your college or university is not listed in the dropdown menu, we apologize but your school is not currently supported by this platform" -->
-      <div class="form-group">
-        <label>Major:</label>
-        <input type="text" class="form-control" v-model="major" />
-      </div>
-      <div class="form-group">
-        <label>Minor:</label>
-        <input type="text" class="form-control" v-model="minor" />
-      </div>
-      <div class="form-group">
-        <label>Graduation Year:</label>
-        <input type="number" class="form-control" v-model="graduationYear" />
-      </div>
-      <div class="form-group">
-        <label>Bio:</label>
-        <input type="text" class="form-control" v-model="bio" />
-      </div>
-      <div class="form-group">
-        <label>Profile Image:</label>
-        <input
-          type="file"
-          class="form-control"
-          v-on:change="setFile($event)"
-          ref="fileInput"
-        />
+      <div v-if="artist">
+        <div class="form-group">
+          <label>Style of Art:</label>
+          <input type="text" class="form-control" v-model="artStyle" />
+        </div>
+        <div class="form-group">
+          <label>Name of School:</label>
+          <input type="text" class="form-control" v-model="college_id" />
+        </div>
+        <!-- Need to make this a dropdown menu -->
+        <!-- also need to add a note saying "If your college or university is not listed in the dropdown menu, we apologize but your school is not currently supported by this platform" -->
+
+        <div class="form-group">
+          <label>Major:</label>
+          <input type="text" class="form-control" v-model="major" />
+        </div>
+        <div class="form-group">
+          <label>Minor:</label>
+          <input type="text" class="form-control" v-model="minor" />
+        </div>
+        <div class="form-group">
+          <label>Graduation Year:</label>
+          <input type="number" class="form-control" v-model="graduationYear" />
+        </div>
+        <div class="form-group">
+          <label>Bio:</label>
+          <input type="text" class="form-control" v-model="bio" />
+        </div>
+        <div class="form-group">
+          <label>Profile Image:</label>
+          <input
+            type="file"
+            class="form-control"
+            v-on:change="setFile($event)"
+            ref="fileInput"
+          />
+        </div>
       </div>
 
       <input type="submit" class="btn btn-primary" value="Submit" />
@@ -84,7 +93,7 @@ export default {
       email: "",
       password: "",
       passwordConfirmation: "",
-      artist: "",
+      artist: "true",
       artStyle: "",
       college_id: "",
       major: "",
