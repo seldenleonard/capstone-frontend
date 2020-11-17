67cb1f00e80b03d0835b4a4fc663d565d46d9520
seldenleonard <selden222@gmail.com>
2020-11-17 12:19:58 -0800

    finishes Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,85 @@
 <template>
   <div class="login">
-    <form v-on:submit.prevent="submit()">
+    <!-- WRAPPER -->
+    <div class="wrapper">
+      <!-- HOME -->
+      <section
+        class="module module-header bg-dark bg-dark-50"
+        data-background="assets/images/section-7.jpg"
+      >
+        <div class="container">
+          <!-- MODULE TITLE -->
+          <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+              <h1 class="module-title font-alt align-center">Forms</h1>
+            </div>
+          </div>
+          <!-- /MODULE TITLE -->
+        </div>
+      </section>
+      <!-- /HOME -->
+
+      <!-- COMPONENTS -->
+      <section class="module">
+        <div class="container">
+          <div class="row">
+            <form
+              v-on:submit.prevent="submit()"
+              class="col-sm-8 col-sm-offset-2"
+            >
+              <h4 class="font-alt m-t-0 m-b-0"><strong>Login</strong></h4>
+              <hr class="divider-w m-t-10 m-b-20" />
+              <div
+                class="alert alert-danger"
+                role="alert"
+                v-for="error in errors"
+              >
+                <strong>Error:</strong> {{ error }}
+              </div>
+              <div class="form-group">
+                <input
+                  class="form-control input-lg"
+                  type="email"
+                  placeholder="Email"
+                  v-model="email"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  class="form-control input-lg"
+                  type="password"
+                  placeholder="Password"
+                  v-model="password"
+                />
+              </div>
+              <button
+                type="submit"
+                class="btn btn-border-d btn-circle"
+                value="Submit"
+              >
+                <i class="fa fa-cog fa-spin"></i>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+      <!-- / COMPONENTS  -->
+
+      <!-- DIVIDER -->
+      <hr class="divider-w" />
+      <!-- /DIVIDER -->
+    </div>
+    <!-- /WRAPPER -->
+
+    <!-- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -->
+
+    <!-- MY OLD CODE -->
+    <!-- <form v-on:submit.prevent="submit()">
       <h1>Login</h1>
       <ul>
-        <li class="text-danger" v-for="error in errors">{{ error }}</li>
-      </ul>
+    <li class="text-danger" v-for="error in errors">{{ error }}</li>
+    </ul>
       <div class="form-group">
         <label>Email:</label>
         <input type="email" class="form-control" v-model="email" />
@@ -14,7 +89,7 @@
         <input type="password" class="form-control" v-model="password" />
       </div>
       <input type="submit" class="btn btn-primary" value="Submit" />
-    </form>
+    </form> -->
   </div>
 </template>
 
