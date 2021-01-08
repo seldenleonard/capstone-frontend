a3f441532f2b8b1e5dc22ac9bc208c1c173d323e
seldenleonard <selden222@gmail.com>
2021-01-08 06:08:43 -0500

    deletes old vue.js code used for reference while incorporating theme in UsersShow.vue

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- NOTE: BECAUSE OF THE WAY I HAVE THIS VUE SET UP, IF AN A USER WHO IS AN ARTIST DOES NOT HAVE AN ARTWORK, THEIR USER SHOW VUE WILL BREAK (BECAUSE THE IMAGES ARE GETTING CALLED AS .URL, SO IF THEY DONT EXIST, THEY CANT BE CALLED)-->
   <div class="users-show">
     <!-- HEADER -->
     <section
@@ -31,20 +30,6 @@
       </div>
     </section>
     <!-- /HEADER -->
-
-    <!-- PORTFOLIO CONTENT (PROFILE IMAGE) -->
-    <!-- <section class="module p-t-0 p-b-0">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12">
-            <p class="text-center">
-              <img v-if="user.artist" :src="user.image_url" alt="" />
-            </p>
-          </div>
-        </div>
-      </div>
-    </section> -->
-    <!-- /PORTFOLIO CONTENT (PROFILE IMAGE) -->
 
     <!-- PORTFOLIO DESCRIPTION -->
     <section class="module-small p-b-30">
@@ -141,7 +126,6 @@
               <div class="post-thumbnail">
                 <router-link :to="`/artworks/${artwork.id}`">
                   <img :src="artwork.images[0].url" alt="" />
-                  <!-- CANNOT FIGURE OUT HOW TO GET ONE IMAGE FOR EACH ARTWORK TO LOAD -->
                 </router-link>
               </div>
               <div class="post-header">
@@ -179,7 +163,6 @@
               </div>
             </div>
           </div>
-
           <!-- /POST ITEM -->
         </div>
         <strong>
@@ -188,55 +171,6 @@
       </div>
     </section>
     <!-- /BLOG GRID -->
-
-    <!-- MY OLD CODE -->
-    <!-- <img v-if="user.artist" :src="user.image_url" alt="" />
-    <h2>{{ user.name }}</h2>
-    <router-link :to="`/colleges/${user.college_id}`">
-      <p v-if="user.artist && user.college">{{ user.college_name }}</p>
-    </router-link>
-    <p v-if="user.artist && user.graduation_year">
-      Class of {{ user.graduation_year }}
-    </p>
-    <p v-if="user.artist && user.major">Major: {{ user.major }}</p>
-    <p v-if="user.artist && user.minor">Minor: {{ user.minor }}</p>
-    <p v-if="user.artist && user.art_style">
-      Style of Art: {{ user.art_style }}
-    </p>
-    <p v-if="user.artist && user.bio">Bio: {{ user.bio }}</p>
-    <p v-if="user.artist">Contact: {{ user.email }}</p>
-    <p v-else>Email: {{ user.email }}</p>
-    <router-link
-      v-if="user.id === $parent.getUserId()"
-      :to="`/users/${user.id}/edit`"
-    >
-      <button>Edit Profile</button>
-    </router-link> -->
-    <!-- <div v-for="artwork in user.artworks">
-      <div v-for="image in artwork.images">
-        <img :src="image.url" alt="" />
-      </div>
-      <router-link :to="`/artworks/${artwork.id}`">
-        <h2>{{ artwork.title }}</h2>
-      </router-link>
-      <p>{{ artwork.year }}</p>
-      <p>{{ artwork.medium }}</p>
-      <p v-if="artwork.dimensions">{{ artwork.dimensions }}</p>
-      <p v-if="artwork.description">{{ artwork.description }}</p>
-      <p v-if="artwork.price">Price: $ {{ artwork.price }}</p>
-      <p>Upvotes: {{ artwork.upvotes_count }}</p>
-      <h5 class="category">Posted {{ relativeDate(artwork.created_at) }}</h5> -->
-    <!-- <router-link
-        v-if="user.id === $parent.getUserId()"
-        :to="`/artworks/${artwork.id}/edit`"
-      >
-        <button>Edit Artwork</button>
-      </router-link> -->
-    <!-- </div> -->
-    <!-- <button v-if="artwork.upvote" v-on:click="destroyUpvote(artwork)">
-        Destroy Upvote
-      </button>
-      <button v-else v-on:click="createUpvote(artwork)">Upvote</button> -->
   </div>
 </template>
 
@@ -252,7 +186,7 @@ export default {
     };
   },
   created: function() {
-    this.$parent.headerImagePath = "college-girl.jpeg"; // I THINK THAT BECAUSE I GOT RID OF THIS IMAGE, I SHOULD GET RID OF THIS, BUT FOR SOME REASON EVERYTHING WORKS OK SO IM NOT GOING TO
+    this.$parent.headerImagePath = "college-girl.jpeg";
     axios
       .get(`/api/users/${this.$route.params.id}`)
       .then((response) => {
@@ -267,32 +201,6 @@ export default {
     relativeDate: function(date) {
       return moment(date).fromNow();
     },
-    // createUpvote: function(artwork) {
-    //   let params = {
-    //     artwork_id: artwork.id,
-    //   };
-    //   axios
-    //     .post("/api/upvotes", params)
-    //     .then((response) => {
-    //       artwork.upvote = true;
-    //       artwork.upvotes_count++;
-    //     })
-    //     .catch((error) => {
-    //       this.errors = error.response.data.errors;
-    //       console.log(this.errors);
-    //     });
-    // },
-    // destroyUpvote: function(artwork) {
-    //   axios
-    //     .delete(`/api/upvotes/${artwork.id}`)
-    //     .then((response) => {
-    //       artwork.upvote = false;
-    //       artwork.upvotes_count--;
-    //     })
-    //     .catch((error) => {
-    //       this.errors = error.response.data.errors;
-    //     });
-    // },
   },
 };
 </script>
