1d3ce4c9a655b93cd63e1c4a20835809d21e3670
seldenleonard <selden222@gmail.com>
2021-01-08 06:01:57 -0500

    deletes old vue.js code used for reference while incorporating theme in Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -86,25 +86,6 @@
       <!-- /DIVIDER -->
     </div>
     <!-- /WRAPPER -->
-
-    <!-- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -->
-
-    <!-- MY OLD CODE -->
-    <!-- <form v-on:submit.prevent="submit()">
-      <h1>Login</h1>
-      <ul>
-    <li class="text-danger" v-for="error in errors">{{ error }}</li>
-    </ul>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" />
-      </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password" />
-      </div>
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form> -->
   </div>
 </template>
 
