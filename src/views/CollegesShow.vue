ae0e294a6b4a8d072d59b76493ab5a94d9fcc973
seldenleonard <selden222@gmail.com>
2021-01-08 05:53:19 -0500

    deletes old vue.js code used for reference while incorporating theme in CollegesShow.vue

--- src/views/CollegesShow.vue
+++ src/views/CollegesShow.vue
@@ -168,36 +168,6 @@
     </section>
     <!-- /BLOG GRID -->
 
-    <!-- MY OLD CODE -->
-    <!-- <img :src="college.image_url" alt="" /> -->
-    <!-- <h1>{{ college.name }}</h1> -->
-    <!-- <p>{{ college.abbreviation }}</p>
-    <p>{{ college.city }}, {{ college.state }}</p>
-    <p>{{ college.bio }}</p>
-    <div v-for="user in college.users">
-      <div v-if="user.artist">
-        <img :src="user.image_url" alt="" />
-        <h2>{{ user.name }}</h2>
-        <p>Class of {{ user.graduation_year }}</p>
-        <p>{{ user.major }} Major</p>
-        <p>{{ user.minor }} Minor</p>
-        <p>Style of Art is {{ user.art_style }}</p>
-        <p>{{ user.bio }}</p> -->
-    <!-- <div v-for="artwork in user.artworks"> -->
-    <!-- <p>{{ artwork.title }}</p> -->
-    <!-- <img :src="image" alt="" /> -->
-    <!-- <div v-for="artwork in user.artworks"> -->
-    <!-- <p>{{ artwork.images.url }}</p> -->
-    <!-- </div> -->
-    <!-- </div> -->
-    <!-- THIS IS AN ATTEMPT TO HAVE SOME OF EACH ARTISTS' ARTWORKS IMAGES SHOW UP, BUT IT IS NOT MVP SO DONT BOTHER FOR NOW -->
-    <!-- <router-link :to="`/users/${user.id}`"> -->
-    <!-- This^ means same as v-bind -->
-    <!-- <button>View Artist</button> -->
-    <!-- </router-link>
-      </div>
-    </div> -->
-
     <!-- /BODY -->
   </div>
 </template>
